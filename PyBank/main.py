78979ab26f99c9887174111807d7109ea5caa4ba
Ka-Ri D Walker <dywayne.walker@gmail.com>
2019-02-26 20:49:51 -0600

    Tested the csv reader file wrote to memory

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,3 +3,7 @@ import  csv
 
 # Go to the CSV location and open file
 csvPath = os.path.join('budget_data.csv')
+with open(csvPath,newline='') as csvFile:
+# Read the tab delimited file on ,
+    csvReader = csv.reader(csvFile, delimiter=',')
+    print(csvReader)
