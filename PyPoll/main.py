856996c47bce1ea5d9502a73a4ea888dc7300efa
Ka-Ri D Walker <dywayne.walker@gmail.com>
2019-02-28 23:47:33 -0600

    output to txt file worked out will read txt file and print to terminal

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -38,27 +38,38 @@ with open(csvPath, newline='') as csvFile:
     #combine data to be broken out
     printData = list(zip(canNames, voteNum,votePer))
 
+    for n in printData:
+        if max(voteNum) == n[1]:
+            canNames.append(n)
+
     # grab the canidates who have recieved votes create a list of them
     canWVotes = canNames[0]
     if len(canNames) > 1:
         for cv in range(1, len(canNames)):
-            canWVotes = canWVotes + "," + canNames[cv]   
-
-# Printed Values for Results 
-electResults = (' Election Results \n',
-'-------------------------\n',
-f'Total Votes: {voteTal}\n',
-'--------------------------\n')
-
-# Terminal Results
-myOutput = ' '.join(electResults)
-print(str(myOutput))
+            canWVotes = canWVotes + "," + str(canNames[cv])
 
 # Create the Text file and Close
 pollResult = open('Election_Results.txt','w')
 
-pollResult.writelines(electResults)
+for cn, vn, vp in zip(canNames, voteNum, votePer):
+    print(cn, vn, vp)
 
+# Printed Values for Results 
+pollResult.writelines(' Election Results \n' +
+'-------------------------\n' +
+f'Total Votes: {voteTal}\n' +
+'--------------------------\n')
+for pollVal in zip(canNames, voteNum, votePer):
+    pollResult.writelines(pollVal[0] + ' : ' + str(pollVal[2]) + '% ('+ str(pollVal[1]) + ') \n')
+pollResult.writelines('-------------------------\n' +
+'Winner: ' +
+'\n-------------------------')
+
+# Terminal Results
+# myOutput = ' '.join(electResults)
+# print(str(myOutput))
+
+# CLose the Txt File
 pollResult.close()
 
 
