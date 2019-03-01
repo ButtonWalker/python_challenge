2b5f2be5ca75c314d384dbcdbc11c1f729cd66c0
Ka-Ri D Walker <dywayne.walker@gmail.com>
2019-02-28 20:18:21 -0600

    read data set and parse and count canidates ensure data returns

---
+++ PyPoll/main.py
@@ -0,0 +1,26 @@
+import os
+import csv
+
+# get csv location
+csvPath = os.path.join('election_data.csv')
+with open(csvPath, newline='') as csvFile:
+
+    # Read the tab delimited file on ,
+    csvReader = csv.reader(csvFile, delimiter=',')
+
+    # Set Values for Canidates name and Vote Count
+    pollRes = {}
+    voteTal = 0
+    canNames = []
+    voteNum = []
+
+    # pares through dataset find names and count
+
+    for row in csvReader:
+        voteTal += 1
+        if row[2] in pollRes.keys():
+            pollRes[row[2]] = pollRes[row[2]] + 1
+        else:
+            pollRes[row[2]] = 1
+
+    print(pollRes)
