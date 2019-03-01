89520e222a4c5442f65db0709795d19faa20f3f8
Ka-Ri D Walker <dywayne.walker@gmail.com>
2019-02-28 20:45:37 -0600

    get the values adn append key and values, Calculate precentage on dict and store

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,13 +8,15 @@ with open(csvPath, newline='') as csvFile:
     # Read the tab delimited file on ,
     csvReader = csv.reader(csvFile, delimiter=',')
 
-    # Set Values for Canidates name and Vote Count
+    # Set Values for Canidates name, Vote Count, Poll results
     pollRes = {}
     voteTal = 0
     canNames = []
     voteNum = []
+    votePer = []
 
-    # pares through dataset find names and count using name in Index 2
+
+    # parse through dataset find names and count using name in Index 2
 
     for row in csvReader:
         voteTal += 1
@@ -23,4 +25,16 @@ with open(csvPath, newline='') as csvFile:
         else:
             pollRes[row[2]] = 1
 
-    print(pollRes)
+    # Cast values from data set parse to canNames and votNum list
+    for key, value in pollRes.items():
+        canNames.append(key)
+        voteNum.append(value)
+
+    # calculate precentage values for canidates
+    for v in voteNum:
+        votePer.append(round(v/voteTal*100,1))
+
+    print(votePer)
+
+
+    
